6514d0da23fbc0f24526693dac82e2c639391b90
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-07 19:03:15 -0400

    Got it down

---
+++ todo.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+
+let todoList = [];
+let maxItems = 10;
+
+while (todoList.length < maxItems) {
+    let item = prompt("Enter a ToDo item: ");
+
+    if (todoList.includes(item)) {
+        console.log("That item is already in the list. Please enter something else.");
+    } else {
+        todoList.push(item);
+    }
+}
+
+console.log("Your ToDo List is:");
+for (let i = 0; i < todoList.length; i++) {
+    console.log((i + 1) + ". " + todoList[i]);
+}
