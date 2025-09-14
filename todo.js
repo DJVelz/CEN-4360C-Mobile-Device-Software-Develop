0af8f3d3fa6094b007ad23a480052e79ab75ad49
DJVelz <165734832+DJVelz@users.noreply.github.com>
2025-09-14 10:22:55 -0400

    loop for program to run

--- todo.js
+++ todo.js
@@ -21,17 +21,14 @@ function displayList() {
         console.log((i+1) + ". " + todoList[i]);
     }
 }
-while (todoList.length < maxItems) {
-    let item = prompt("Enter a ToDo item: ");
 
-    if (todoList.includes(item)) {
-        console.log("That item is already in the list. Please enter something else.");
-    } else {
-        todoList.push(item);
+// Run the program
+function runTodoApp() {
+    while (todoList.length < maxItems) {
+        let item = prompt("Enter a ToDo Item: ");
+        addItem(item);
     }
+    displayList();
 }
 
-console.log("Your ToDo List is:");
-for (let i = 0; i < todoList.length; i++) {
-    console.log((i + 1) + ". " + todoList[i]);
-}
+runTodoApp();
